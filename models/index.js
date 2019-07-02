b6e7e9efbb04008e697f4aea1c5191a6b09b20ea
EC2 Default User <ec2-user@ip-172-31-34-26.ec2.internal>
2019-07-02 21:21:21 +0000

    cluster connect update

--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
+var password = encodeURIComponent("Shre@21595");
+var uri = "mongodb+srv://shreyank:"+ password +"@cluster0-nlrdk.mongodb.net/todoapp?retryWrites=true&w=majority";
+var localUri = "mongodb://localhost/todo-api";
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api', { useNewUrlParser: true });
+mongoose.connect(uri, { useNewUrlParser: true });
 
 mongoose.Promise = Promise;
 
